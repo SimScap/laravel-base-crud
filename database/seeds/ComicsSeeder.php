0434b1b2640f8bc7af9bb8300accfd4966684246
SimoneScapin <simonescap87@gmail.com>
2022-05-17 16:43:22 +0200

    bonus fatto ma da finire il resto

--- database/seeds/ComicsSeeder.php
+++ database/seeds/ComicsSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Comic;
 class ComicsSeeder extends Seeder
 {
     /**
@@ -12,5 +12,16 @@ class ComicsSeeder extends Seeder
     public function run()
     {
         $comics = config("comics");
+        foreach( $comics as $comic){
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+        }
     }
 }
