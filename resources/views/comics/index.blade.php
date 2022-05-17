b249235721e9f2299bc147fb94d15a7d8865807c
SimoneScapin <simonescap87@gmail.com>
2022-05-17 17:07:41 +0200

    creata tabella

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,6 +6,35 @@
 <main>
     <div class="container-fluid text-center">
         <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Thumb</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Series</th>
+                            <th scope="col">Sale Date</th>
+                            <th scope="col">Type</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($comics as $comic)
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>{{ $comic->title}}</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                        </tr>
+                        @endforeach                  
+                    
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </main>
